fe93fc415bfc7fbc2f739d7f05544f5a8601e02e
msadeghirad <m3hran.sadeghi@gmail.com>
2026-08-10 12:12:14 +0330

    Update script.js

--- script.js
+++ script.js
@@ -272,14 +272,7 @@ function startSlider(){
 }
 
 /* ============================
-   QR Code
-============================ */
-
-function generateQR(){
-    const currentUrl = window.location.href;
-    const qrUrl = "https://api.qrserver.com/v1/create-qr-code/?size=160x160&data=" + encodeURIComponent(currentUrl);
-    document.getElementById("qrImage").src = qrUrl;
-}
+  
 
 /* ============================
    پنل مدیریت
